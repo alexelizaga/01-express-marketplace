c255352011d014c79d5fbc0b1aea5c6126470b00
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-05-28 17:29:22 +0200

    1

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({
-        ok: true
-    });
+// uriHome: 'marketplace'
+// http://localhost:4000/v1/authorize?response_type=code&client_id=marketplace&scope=OPENID&id_token_hint=aaaabbbbccccdddd
+// http://localhost:4000/v1/authorize?response_type=code&client_id=marketplace&redirect_uri=marketplace&scope=OPENID&id_token_hint=aaaabbbbccccdddd
+
+// uriPurchase: 'shoppingcard'
+// http://localhost:4000/v1/authorize?response_type=code&client_id=marketplace&redirect_uri=shoppingcard&scope=OPENID&id_token_hint=aaaabbbbccccdddd
+
+app.get('/v1/authorize', (req, res) => {
+    const responseType = req.query.response_type;
+    const clientId = req.query.client_id;
+    const redirectUri = req.query.redirect_uri;
+    const scope = req.query.scope;
+    const idTokenHint = req.query.id_token_hint;
+
+    if(responseType !== 'code' || clientId !== 'marketplace' || scope !== 'OPENID' || !idTokenHint) {
+        res.json({
+            ok: false,
+            responseType,
+            clientId,
+            redirectUri,
+            scope,
+            idTokenHint
+        });
+    };
+
+    if (!redirectUri || redirectUri === 'marketplace') {
+        res.status(301).redirect(`https://tsmarketplacedemo.web.app?at=${idTokenHint}`);
+    };
+
+    res.status(301).redirect(`https://tsmarketplacedemo.web.app/${redirectUri}?at=${idTokenHint}`);
 });
 
 app.listen(4000, () => {
