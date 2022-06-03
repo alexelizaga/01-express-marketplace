b59c215cd19485f0acb7c116fd1122fedd554e6d
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2022-06-03 06:49:24 +0200

    FIX: Add error code to response

--- controllers/authorize.js
+++ controllers/authorize.js
@@ -10,7 +10,7 @@ const redirect = ( req, res = response ) => {
     const jws = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJpcXVpamFkMiIsInJvbGVzIjpbIlJPTEVfVVNFUiIsIlJPTEVfQURNSU4iXSwiaXNzIjoiVm9kYWZvbmVEWEwiLCJleHAiOjE2Mjk2MTc2OTEsInVzZXJQcm9maWxlIjp7Imxhc3RQYXNzQ2hhbmdlRGF0ZSI6MCwibmFtZSI6IlFVSUpBREEsIElTTUFFTCIsImlzQWRtaW4iOnRydWUsInByZXBhaWQiOmZhbHNlfX0.mAPa9Kq9QS4JzRUb1liu-mSPKt7nb94aISy0lYqzXE-3E1_E6S2Gbl-b28Y39kNIa0rhiNYIpRENqKwO5Wt3EhHS0UcRQCLQVP9uU8y0N14Zw3C1X-DyHu1xyiWYQjAOtz87fNT18vQYcAyTwgC5pcfMo-mGhwhtMe0ZaVrOUelprGpT0c3kXbQ-nAPnpQoDwqiktEoo-3OahVMPuulduN5WWLkUd5-ckcLl1Ys_8HikTt9FX2pMkRIqr8Pb6tX-AMbN50TzWJuHMa50eAL4bYcoWOerwRHqUuBIn9L9XB5kWTcCB1N7RxnALe23VOHIc2ko2jTXE-rWJGMO9u1zcw'
 
     if(responseType !== 'code' || clientId !== 'marketplace' || scope !== 'OPENID' || !idTokenHint) {
-        res.json({
+        res.status(400).json({
             ok: false,
             responseType,
             clientId,
